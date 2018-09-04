a03224556907e27d0f83b5f8e4de94a7faef1740
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2018-09-04 10:31:05 -0700

    Adding search bar functionality.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './SearchBar.css';
 
-let sortByOptions = {
+const sortByOptions = {
   'Best Match': 'best_match',
   'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
+  'Most Reviewed': 'review_count',
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption) {
+      return 'active';
+    } else {
+      return '';
+    }
+  },
+  handleSortByChange(sortByOption) {
+    this.setState({sortBy: sortByOption})
+  }
 }
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+  }
+
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
